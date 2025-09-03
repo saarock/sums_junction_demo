7206101ea723208599aeff17b61df62e2a9451a7
saarock <saarock4646@gmail.com>
2025-09-03 09:10:56 +0545

    corrected the segment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import { Button } from "./components/ui/button";
-import { Badge } from "./components/ui/badge";
-import { ImageWithFallback } from "./components/figma/ImageWithFallback";
 import { Navigation } from "./components/Navigation";
-import { ContactForm } from "./components/ContactForm";
-
 
 import {
   EventDetails,
@@ -11,24 +6,25 @@ import {
   PartnerLogos,
   Sponsors,
   WhatIsJunctionX,
-  WhyKathmandu,
 } from "./components/sections";
 import Footer from "./components/Footer";
 import { Countdown } from "./components/Countdown";
 
-
 export default function App() {
   return (
-    <div className="min-h-screen bg-black relative">
+    <>
       <Navigation />
-      <HeroSection />
-      <Countdown/>
-      <PartnerLogos/>
-      <WhatIsJunctionX />
-      {/* <WhyKathmandu /> */}
-      <EventDetails />
-      <Sponsors />
+
+      <main className="min-h-screen bg-black relative">
+        <HeroSection />
+        <Countdown />
+        <PartnerLogos />
+        <WhatIsJunctionX />
+        {/* <WhyKathmandu /> */}
+        <EventDetails />
+        <Sponsors />
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
