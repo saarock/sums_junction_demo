17143032e9f832b8e4739bddacd72dd832bb9e3c
saarock <saarock4646@gmail.com>
2025-12-01 17:30:11 +0545

    date update

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -17,7 +17,8 @@ export function Countdown() {
   const countdownRef = useRef(null);
 
   useEffect(() => {
-    const targetDate = new Date("2026-05-29T00:00:00").getTime();
+const targetDate = new Date("2026-05-29T00:00:00").getTime();
+
 
     const updateCountdown = () => {
       const now = new Date().getTime();
